d378b13f9261682813c9ed9adb2eac344ffb1f33
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-07-26 15:28:55 +0500

    add source code

---
+++ Client/src/app/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <h1 className="text-3xl font-black text-gray-300">
+        Welcome to the TeleAgent Application
+      </h1>
+    </div>
+  );
+};
+
+export default page;
