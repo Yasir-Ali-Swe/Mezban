fda9f988c3a4c14720aa7510c129afb080569416
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-08-04 22:09:49 +0500

    add deals routes

---
+++ Client/src/app/(dashboard)/deals/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <h1 className='text-2xl font-bold'>Edit Deal Page</h1>
+        </div>
+    )
+}
+
+export default page
