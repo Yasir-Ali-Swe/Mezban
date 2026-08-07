1d400acc4cbdb37369ec4a2513c1cd7bdd3ac900
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-08-07 20:18:34 +0500

    remove onboarding business info page

--- Client/src/app/onboarding/business-info/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-    return (
-        <div className="flex items-center justify-center h-screen">
-            <h1 className='text-2xl font-bold'>Business Info</h1>
-        </div>
-    )
-}
