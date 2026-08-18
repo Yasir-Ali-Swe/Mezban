09dc7c87d412641d2eecdcb8db252222e8fdcb30
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-08-18 20:58:20 +0500

    update server main entry point

--- server/main.js
+++ server/main.js
@@ -3,12 +3,14 @@ import cors from "cors";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import { PORT } from "./config/env.js";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
+app.use(clerkMiddleware());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port localhost:${PORT}`);
