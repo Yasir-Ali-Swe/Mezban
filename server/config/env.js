5d24b70f8717c666eb0d575664f9e0434a65a60c
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-08-18 20:58:11 +0500

    update server env configuration

--- server/config/env.js
+++ server/config/env.js
@@ -1,6 +1,15 @@
 import "dotenv/config";
 
 const PORT = process.env.PORT;
-const DATABASE_URL = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
+const GOOGLE_GENAI_API_KEY = process.env.GOOGLE_GENAI_API_KEY;
+const CLERK_PUBLISHABLE_KEY = process.env.CLERK_PUBLISHABLE_KEY;
+const CLERK_SECRET_KEY = process.env.CLERK_SECRET_KEY;
 
-export { PORT, DATABASE_URL };
+export {
+  PORT,
+  DATABASE_URL,
+  GOOGLE_GENAI_API_KEY,
+  CLERK_PUBLISHABLE_KEY,
+  CLERK_SECRET_KEY,
+};
