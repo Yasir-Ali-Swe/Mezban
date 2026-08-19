6dc22b70bbb582ce0e99200a2c659a6e1e43f5da
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-08-19 15:51:23 +0500

    update onboarding business knowledge page

--- Client/src/app/onboarding/business-knowledge/page.jsx
+++ Client/src/app/onboarding/business-knowledge/page.jsx
@@ -1,11 +1,10 @@
 import BusinessKnowledgeForm from '@/components/business-data/knowledge';
-
 import React from 'react'
 
 const page = () => {
     return (
         <>
-            <BusinessKnowledgeForm />
+            <BusinessKnowledgeForm mode="onboarding" />
         </>
     )
 }
