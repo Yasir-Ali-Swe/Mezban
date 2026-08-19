f46b70eb00553ffeaa4eafb5344af2e9f261b324
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-08-19 15:54:37 +0500

    update server env configuration

--- server/config/env.js
+++ server/config/env.js
@@ -5,11 +5,16 @@ const DATABASE_URL = process.env.DATABASE_URL;
 const GOOGLE_GENAI_API_KEY = process.env.GOOGLE_GENAI_API_KEY;
 const CLERK_PUBLISHABLE_KEY = process.env.CLERK_PUBLISHABLE_KEY;
 const CLERK_SECRET_KEY = process.env.CLERK_SECRET_KEY;
-
+const cloud_name = process.env.cloud_name;
+const api_key = process.env.api_key;
+const api_secret = process.env.api_secret;
 export {
   PORT,
   DATABASE_URL,
   GOOGLE_GENAI_API_KEY,
   CLERK_PUBLISHABLE_KEY,
   CLERK_SECRET_KEY,
+  cloud_name,
+  api_key,
+  api_secret,
 };
