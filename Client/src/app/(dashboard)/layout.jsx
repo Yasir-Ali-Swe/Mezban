d0ab1fb2dce3a51db0eb92ffe0be9dcc4041a54b
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-08-19 15:49:57 +0500

    update dashboard layout

--- Client/src/app/(dashboard)/layout.jsx
+++ Client/src/app/(dashboard)/layout.jsx
@@ -3,6 +3,7 @@ import { cookies } from 'next/headers'
 import { Sidebar } from "@/components/shared/Sidebar"
 import { Navbar } from "@/components/shared/Navbar"
 import SidebarProvider from '@/components/shared/SidebarProvider'
+import { AuthGuard } from '@/components/providers/AuthGuard'
 
 const SIDEBAR_COOKIE_NAME = 'sidebar_state';
 const Layout = async ({ children }) => {
@@ -11,17 +12,19 @@ const Layout = async ({ children }) => {
     const defaultOpen = sidebarCookie === undefined ? true : sidebarCookie === 'true';
 
     return (
-        <SidebarProvider defaultOpen={defaultOpen}>
-            <div className="flex h-screen w-full overflow-hidden">
-                <Sidebar />
-                <div className="flex flex-1 flex-col overflow-hidden">
-                    <Navbar />
-                    <main className="flex-1 overflow-y-auto p-4 ">
-                        {children}
-                    </main>
+        <AuthGuard mode="dashboard">
+            <SidebarProvider defaultOpen={defaultOpen}>
+                <div className="flex h-screen w-full overflow-hidden">
+                    <Sidebar />
+                    <div className="flex flex-1 flex-col overflow-hidden">
+                        <Navbar />
+                        <main className="flex-1 overflow-y-auto p-4 ">
+                            {children}
+                        </main>
+                    </div>
                 </div>
-            </div>
-        </SidebarProvider>
+            </SidebarProvider>
+        </AuthGuard>
     )
 }
 
